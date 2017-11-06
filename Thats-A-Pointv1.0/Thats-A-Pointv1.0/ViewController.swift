83f8ebf299c8671e23b2298657f5e49a1dc0aeca
Ross Krueger <rkrueger13@winona.edu>
2017-11-06 16:19:29 -0600

    Created Project
    Thats-A-Point version 1.0 generated.
    This is far ahead of dev. schedule.

---
+++ Thats-A-Pointv1.0/Thats-A-Pointv1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Thats-A-Pointv1.0
+//
+//  Created by Krueger, Ross L on 11/6/17.
+//  Copyright © 2017 Krueger, Ross L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
